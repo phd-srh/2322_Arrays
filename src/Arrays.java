6e9bf4f16ce40accb2aa34272809c156bd293570
Dr. Dillinger <peter.dillinger@srh.de>
2022-12-08 15:41:56 +0100

    Suchfunktion implementiert!

--- src/Arrays.java
+++ src/Arrays.java
@@ -1,5 +1,25 @@
 public class Arrays {
+
+    // Methoden mit Arrays...
+    public static boolean sucheImArray(int[] array, int datum) {
+        for (int i = 0; i < array.length ; i++ ) {
+            if (array[i] == datum) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static int minimalerWertImArray(int[] array) {
+        // TODO
+    }
+
     public static void main(String[] args) {
-        
+        int[] datenreihe = { 17, 3, 29, 22, 5, 11, 9, 13, 7, 12 };
+
+        System.out.println("Die 12 ist in der Datenreihe: " +
+                sucheImArray(datenreihe, 12) );
+        System.out.println("Die 23 ist in der Datenreihe: " +
+                sucheImArray(datenreihe, 23) );
     }
 }
