594325909d0bbae9b98a76bcb0bdb6c35fac8440
Dr. Dillinger <peter.dillinger@srh.de>
2022-12-12 11:38:38 +0100

    istArraySortiert(...) läuft ...

--- src/Arrays.java
+++ src/Arrays.java
@@ -20,12 +20,16 @@ public class Arrays {
     }
 
     public static boolean istArraySortiert(int[] array) {
-        // TODO
-        return false;
+        for (int i=1; i < array.length; i++) {
+            if (array[i-1] > array[i])
+                return false;
+        }
+        return true;
     }
 
     public static void main(String[] args) {
         int[] datenreihe = { 17, 3, 29, 22, 5, 11, 9, 13, 7, 12 };
+        int[] sortierteFolge = { 3, 5, 5, 7, 11, 22, 29, 37, 55, 103 };
 
         System.out.println("Die 12 ist in der Datenreihe: " +
                 sucheImArray(datenreihe, 12) );
@@ -33,5 +37,9 @@ public class Arrays {
                 sucheImArray(datenreihe, 23) );
         System.out.println("Kleinster Wert: " +
                 minimalerWertImArray(datenreihe));
+        System.out.println("Ist das Array (datenreihe )sortiert? " +
+                istArraySortiert(datenreihe));
+        System.out.println("Ist das Array (sortierteFolge )sortiert? " +
+                istArraySortiert(sortierteFolge));
     }
 }
