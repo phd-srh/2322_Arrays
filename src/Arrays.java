12a670cb8892ba01348b5baaf2b1bab9a9689017
Dr. Dillinger <peter.dillinger@srh.de>
2022-12-12 11:56:56 +0100

    Neue Aufgabe: entferneZahlAusArray

--- src/Arrays.java
+++ src/Arrays.java
@@ -27,6 +27,42 @@ public class Arrays {
         return true;
     }
 
+    public static void sortierArrayBubbleSort(int[] array) {
+        boolean wurdenDatenGetauscht;
+        do {
+            wurdenDatenGetauscht = false;
+            for (int i = 1; i < array.length; i++) {
+                if (array[i - 1] > array[i]) {
+                    // swap - zwei Elemente im Array tauschen
+                    int temp = array[i - 1];
+                    array[i - 1] = array[i];
+                    array[i] = temp;
+                    wurdenDatenGetauscht = true;
+                }
+            }
+        } while (wurdenDatenGetauscht);
+    }
+
+    public static void ausgabeArray(int[] array) {
+        System.out.print("[");
+        for (int i=0; i < array.length; i++) {
+            if (i>0) System.out.print(", ");
+            System.out.print(array[i]);
+        }
+        System.out.println("]");
+    }
+
+    /* Neue Aufgabe: Entferne
+     *  z.B. array = [ 3, 7, 13, 25 ], zahl = 13
+     *  Ergebnis = [ 3, 7, 25 ]
+     * Achtung: Es soll ein neues(!) Array generiert werden und nicht
+     *          das übergebene manipuliert!!
+     */
+    public static int[] entferneZahlAusArray(int[] array, int zahl) {
+        // TODO
+        return array; // <- nur Platzhalter, das stimmt nicht!
+    }
+
     public static void main(String[] args) {
         int[] datenreihe = { 17, 3, 29, 22, 5, 11, 9, 13, 7, 12 };
         int[] sortierteFolge = { 3, 5, 5, 7, 11, 22, 29, 37, 55, 103 };
@@ -41,5 +77,8 @@ public class Arrays {
                 istArraySortiert(datenreihe));
         System.out.println("Ist das Array (sortierteFolge )sortiert? " +
                 istArraySortiert(sortierteFolge));
+        sortierArrayBubbleSort(datenreihe);
+//        java.util.Arrays.sort( datenreihe ); // <- was ist das denn???
+        ausgabeArray(datenreihe);
     }
 }
